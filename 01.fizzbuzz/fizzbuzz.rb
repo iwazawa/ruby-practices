6aacefb09d626d62fd3606a951974f897a8e3a06
ryo <iwazawa.ryo0829@gmail.com>
2021-06-21 17:41:32 +0900

    メンターさんからのフィードバックを反映してコード修正
    Rubyのカウントアップではwhileを使うケースは少ないため、範囲オブジェクトに対するeachメソッドを使うようにコードを修正しました。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,18 +1,11 @@
-x = 1
-while x <= 20
-  # 変数xを3で割った時の余りを変数yに代入
-  y = x % 3
-  # 変数xを5で割った時の余りを変数zに代入
-  z = x % 5
-    
-  if y == 0 && z != 0 #3の倍数だが、5の倍数ではないとき
-    puts "Fizz"
-  elsif y != 0 && z == 0  #5の倍数だが、3の倍数ではとき
+(1..20).each do |x|
+  if x % 15 == 0  # 15で割り切れるとき
+    puts "FizzzBuzz"
+  elsif x % 5 == 0  # 5で割り切れるとき
     puts "Buzz"
-  elsif y == 0 && z == 0  #3と5両方の倍数のとき
-    puts "FizzBuzz"
-  else  #それ以外
-    puts x
+  elsif x % 3 == 0  # 3で割り切れるとき
+    puts "Fizz"
+  else
+    puts x  # それ以外
   end
-  x += 1  #変数xに1を加算
 end
